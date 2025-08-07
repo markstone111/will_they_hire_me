0e93c2c5f5935965d6ca15019527ce748d1d909a
Nikunj Maheshwari <nikunjnehu@gmail.com>
2025-08-07 20:24:23 +0530

    feat: Move Firebase config to environment variables

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyClq3tJni_CTh3WpAcXD48Vd7z31Nq-qMs",
-  authDomain: "willtheyhireme-68c18.firebaseapp.com",
-  projectId: "willtheyhireme-68c18",
-  storageBucket: "willtheyhireme-68c18.firebasestorage.app",
-  messagingSenderId: "462142531736",
-  appId: "1:462142531736:web:b57f65867fbae714b688ed",
-  measurementId: "G-0EB6X31CWX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
